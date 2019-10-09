1d34803759ec0f9a58e448b08a941761bf9a269d
Valeriy Molchanov <valeriy.molchanov.77@gmail.com>
2019-10-09 23:29:27 +0300

    SIGINT

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hello Node.js")
+
+setTimeout(
+    () => console.log("see you"),
+    2000
+)
+
+process.on('SIGINT', handleSignal)
+process.on('SIGTERM', handleSignal)
+
+function handleSignal(signal) {
+    console.log(`Received signal ${signal}`)
+    console.log("bye bye")
+    process.exit(0)
+}
