118b14191db2ddfdcafdb91ba0b54b8dfa3ced98
Valeriy Molchanov <valeriy.molchanov.77@gmail.com>
2019-10-10 12:10:25 +0300

    npm i @koa/router

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const Koa = require('koa')
+const Router = require('@koa/router');
 
-console.log("Hello Node.js")
-
-setTimeout(
-    () => console.log("see you"),
-    2000
-)
+const app = new Koa();
+const router = new Router();
 
 process.on('SIGINT', handleSignal)
 process.on('SIGTERM', handleSignal)
 
 function handleSignal(signal) {
     console.log()
-    console.log(`Received signal ${signal}`)
+    console.log(`received signal ${signal}`)
     console.log("bye bye")
     process.exit(0)
 }
+
+router
+    .get('/', (ctx, next) => {
+        ctx.body = 'Welcome friend!';
+    });
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(3000);
